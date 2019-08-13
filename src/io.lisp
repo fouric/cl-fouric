577525903ca4cffedef8561104d7d9792f2d3dfc
fouric <fouric@protonmail.com>
2019-08-13 16:41:58 -0400

    remove WITH-STANDARD-IO-SYNTAX

--- src/io.lisp
+++ src/io.lisp
@@ -4,15 +4,13 @@
   (with-open-file (in filename
                       :direction :input
                       :if-exists :supersede)
-    (with-standard-io-syntax
-      (read in))))
+    (read in)))
 
 (defun write-file (filename object)
   (with-open-file (out filename
                        :direction :output
                        :if-exists :supersede)
-    (with-standard-io-syntax
-      (print object out))))
+    (print object out)))
 
 (defun file-lines (filename &optional filter)
   "read in the named file into a list of strings, one per line, and optionall call FILTER on each line"
